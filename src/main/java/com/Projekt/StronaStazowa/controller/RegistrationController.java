ca39081e61244f5a3791c87e1b07a26ba3c425f6
JakubCzul <jc305354@student.polsl.pl>
2024-06-09 20:31:32 +0200

    StronaRejestracji

--- src/main/java/com/Projekt/StronaStazowa/controller/RegistrationController.java
+++ src/main/java/com/Projekt/StronaStazowa/controller/RegistrationController.java
@@ -1,14 +1,39 @@
 package com.Projekt.StronaStazowa.controller;
 
+import com.Projekt.StronaStazowa.model.User;
+import com.Projekt.StronaStazowa.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/registration")
 public class RegistrationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
     @GetMapping
     public String registrationPage() {
         return "registrationPage";
+    }
+
+    @PostMapping
+    public String registerUser(@RequestParam("login") String login,
+                               @RequestParam("email") String email,
+                               @RequestParam("password") String password) {
+
+        User user = new User();
+        user.setLogin(login);
+        user.setEmail(email);
+        user.setPassword(password);
+
+        userRepository.save(user);
+
+        return "redirect:/login";
     }
 }
---
+++ src/main/java/com/Projekt/StronaStazowa/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.Projekt.StronaStazowa.repository;
+
+import com.Projekt.StronaStazowa.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
